cc7f9ca33ae09adf3980b28523ffa72f00c88cba
Lucas Peng <lucasp0927@gmail.com>
2010-07-01 01:12:55 +0800

    add version with edit function.

---
+++ exp1_w_edit/debounce.v
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Pushbutton Debounce Module 
+//
+///////////////////////////////////////////////////////////////////////////////
+
+module debounce (/*AUTOARG*/
+   // Outputs
+   clean,
+   // Inputs
+   clks, noisy
+   );
+   input  clks, noisy;
+   output clean;
+
+   parameter NDELAY = 650000;
+   parameter NBITS = 20;
+
+   reg [NBITS-1:0] count;
+   reg xnew, clean;
+
+   always @(posedge clks)
+     if (!noisy) begin xnew <= noisy; clean <= noisy; count <= 0; end
+     else if (noisy != xnew) begin xnew <= noisy; count <= 0; end
+     else if (count == NDELAY) clean <= xnew;
+     else count <= count+1;
+
+endmodule
