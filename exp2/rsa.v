97200e288bdb171a8a6052d52892945847bfe108
Lucas Peng <lucasp0927@gmail.com>
2010-07-04 21:16:18 +0800

    start exp2

--- exp2/rsa.v
+++ exp2/rsa.v
@@ -1,10 +1,24 @@
-module rsa (clk,a,b,c);
-   input clk;
-   input [3:0] a,b;
-   output [3:0] c;
-   reg [3:0] 	c;
-
-   always @ (posedge clk)begin
-      c = a;	
-   end
+module rsa (/*AUTOARG*/
+   // Outputs
+   ready, data_o,
+   // Inputs
+   clk, reset, we, oe, start, reg_sel, addr, data_i
+   );
+   input clk,reset,we,oe,start;
+   input [1:0] reg_sel;
+   input [4:0] addr;
+   input [7:0] data_i;
+   output      ready;
+   output [7:0] data_o;
+   integer 	addr_num;
+   reg [255:0] 	a[3:0]; //a[0] = a[1]^a[2] mod a[3]
+   
+   always @ (posedge clk)
+     begin
+	addr_num <= addr;
+	if (we == 0) begin
+	   a[reg_sel][8*(addr_num+1)-1:(8*addr_num)] = data_i;
+	end 
+     end
+   
 endmodule
