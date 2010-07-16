eaf0c60c4ca48eeaf3cdd0cfbbe2d1de75653960
hanklee2008 <hanklee2008@hanklee2008-laptop.(none)>
2010-07-16 19:28:46 +0800

    slow

--- exp3/dac.v
+++ exp3/dac.v
@@ -3,22 +3,23 @@ module dac (slowmethod, slow, fast, play, bclk, daclrc, dacdat, addr, read ,data
    input bclk;
    input daclrc;
    input [15:0] data;
-   input [3:0] 	fast;
-   input 	slowmethod;
-   input [3:0] 	slow;
+   input [3:0] slow,fast;
+   input slowmethod;
+
+
    output 	read;
    output 	dacdat;
    output [17:0] addr ;
+
+   reg [4:0] 	 counter;
    /*AUTOREG*/
    // Beginning of automatic regs (for this module's undeclared outputs)
-   reg [4:0] 	 counter;
-   reg [4:0] 	 counter_addr;
-   reg 		 dacdat;
-   reg 		 read;
-   reg [3:0] 	 counter2;
+   reg			dacdat;
+   reg			read;
+   reg [3:0]		finish;
    // End of automatics
    /*AUTOWIRE*/
-   reg [17:0] 	 addr_buffer;
+   reg [17:0] 		addr_buffer;
    assign addr = play?addr_buffer:18'bzzzzzzzzzzzzzzzzzz;
    
    always @(posedge bclk) begin
@@ -26,25 +27,27 @@ module dac (slowmethod, slow, fast, play, bclk, daclrc, dacdat, addr, read ,data
 	read <= 0;
       else begin
 	 read <= 1;
-	 if (slowmethod == 0) begin
-	 	if (daclrc == 0 && counter_addr != 5'd16) begin
-			if(counter == 5'd15) begin
-				counter <= 0;
-				counter2 <= counter2 + 1;
-			end else
-				counter <= counter+1;
-	    		dacdat <= data[counter];
-	    		if(counter2 == slow-1)begin
-				counter2 <= 0;
-				counter_addr <= counter_addr+1;
-	    		end   
-	 	end else
-	   	  dacdat <= 0;
-	end
-	 if (counter_addr == 5'd16 && daclrc == 1) begin
-	    counter_addr <= 0;
-	    counter <=0;
-	    addr_buffer <= addr_buffer+fast;
+	 if (daclrc == 0 && counter != 5'd16) begin
+            if (finish < slow-1) begin
+	       	if (counter == 5'd15) begin
+		   counter <= 0 ;    
+		   dacdat <= data[counter] ;
+		end else begin
+	    	   counter <= counter+1;
+	    	   dacdat <= data[counter];
+		end
+	    end else begin
+		counter <= counter+1;
+	    	dacdat <= data[counter];
+	    end
+	 end else
+	    dacdat <= 0;
+
+
+
+	 if (counter == 5'd16 && daclrc == 1) begin
+	    counter <= 0;
+	    addr_buffer <= addr_buffer+1;
 	 end
       end
    end
